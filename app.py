a0e6c346f60272119331b4ddb2506cd1a155f42d
vmarinacosta05-lab <vmarinacosta05@gmail.com>
2026-03-12 13:55:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <1.0:
             st.write( 'Es un sentimiento Positivo 😊')
-           with open('Cute Doggie.json') as source:
+            with open('Cute Doggie.json') as source:
               animation=json.load(source)
             st.lottie(animation,width=350)
         elif x >-1 and x < 0:
