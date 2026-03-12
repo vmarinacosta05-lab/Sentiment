ae36fdec7fb456e20185bfe44b72d98ff3861c77
vmarinacosta05-lab <vmarinacosta05@gmail.com>
2026-03-12 13:55:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ with st.expander('Analizar texto'):
             st.lottie(animation,width=350)
         elif x >-1 and x < 0:
             st.write( 'Es un sentimiento Negativo 😔')
-           with open('Sad animation.json') as source:
+            with open('Sad animation.json') as source:
               animation=json.load(source)
             st.lottie(animation,width=350)
         else:
